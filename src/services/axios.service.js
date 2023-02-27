5e18d7e12c815e0a5a0ee6a05b439c0e473b15e4
Ruskon13 <ruslankondra14@gmail.com>
2023-02-27 09:35:13 +0200

    Final

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-
 import {baseURL} from "../configs";
 
 const axiosService = axios.create({baseURL});
 
-const _accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWQ0Nzc3ZmYxOWIyZTQ1OWJiNjMxNDYwYjc2MDcyZiIsInN1YiI6IjYzZjAyNGZkNTI0OTc4MDA4ZWZjOWNlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k-qxANdz4w8JjmaZ6qoyiiQF0kH6wTcCIP3R8VX-B1I';
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWQ0Nzc3ZmYxOWIyZTQ1OWJiNjMxNDYwYjc2MDcyZiIsInN1YiI6IjYzZjAyNGZkNTI0OTc4MDA4ZWZjOWNlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k-qxANdz4w8JjmaZ6qoyiiQF0kH6wTcCIP3R8VX-B1I'
 
 axiosService.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${_accessToken}`
+    config.headers.Authorization = `Bearer ${token}`
     return config
-});
+})
 
 export {
     axiosService
